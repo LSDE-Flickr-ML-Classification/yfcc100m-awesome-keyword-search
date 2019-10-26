8c884d5fc443c5e11cdb8b99f52b5b17dda353c3
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-10-26 23:53:16 +0200

    Final app layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
   <div id="app">
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <SearchBar/>
+    <div class="container-fluid p-0">
+      <!-- Image and text -->
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="https://github.com/LSDE-Flickr-ML-Classification">About this</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <SearchBar/>
+          </div>
         </div>
       </div>
     </div>
@@ -28,6 +40,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
+
+/*body { padding-top: 40px; }
+
+@media screen and (max-width: 768px) {
+  body { padding-top: 0px; }
+}*/
+
 </style>
